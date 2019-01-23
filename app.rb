75dc4d0be18b4b4a63bc8d94a6d09cddc07fb82c
boggs <hello@boggs.xyz>
2019-01-23 10:30:32 +0800

    feat(app): Create multiple resources for responses

--- app.rb
+++ app.rb
@@ -5,11 +5,25 @@ require 'sinatra/json'
 set :bind, '0.0.0.0'
 
 get '/*' do
-  path = request.path_info
+  resource_key = ENV['resource']
+  resource =
+    case resource_key
+    when 'random_engg'
+      %w(Boggs Rodj JP Jeffrey Kiko Carissa Maynard Diomar Ben Hayden)
+    when 'random_letter'
+      ('a'..'z').to_a + ('A'..'Z').to_a
+    when 'dice_roll'
+      (1..6).to_a
+    else # random_number
+      (0..100).to_a
+    end
 
   json(
-    message: "Welcome from kong-sample-service on Sinatra!",
-    path: path,
-    env: request.env
+    message: resource.sample,
+    path: request.path_info,
+    debug: {
+      env: request.env,
+      resource_key: resource_key
+    }
   )
 end
